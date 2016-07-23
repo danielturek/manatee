e88e0ad5517084bbcfdf59d449170103a6e65a00
danielturek <danielturek@gmail.com>
2016-07-23 03:49:11 -0400

    defs

--- code/defs.R
+++ code/defs.R
@@ -154,3 +154,28 @@ sampler_RW_multinomial <- nimbleFunction(
     ), where = getLoadingNamespace()
 )
 
+## utility for plotting MCMC samples
+samplesPlot <- function(samples, ind=1:ncol(samples), burnin=NULL, width=7, height=4, legend=TRUE, legend.location='topright') {
+    dev.new(height=height, width=width)
+    par(mfrow=c(1,2), cex=0.7, cex.main=1.5, lab=c(3,3,7), mgp=c(0,0.6,0), mar=c(2,1,2,1), oma=c(0,0,0,0), tcl=-0.3, yaxt='n', bty='l')
+    samples <- samples[, ind, drop=FALSE]
+    if(!is.null(burnin))
+        samples <- samples[(burnin+1):dim(samples)[1], , drop=FALSE]
+    nparam <- ncol(samples)
+    rng <- range(samples)
+    plot(1:nrow(samples), ylim=rng, type='n', main='Traceplots', xlab='', ylab='')
+    for(i in 1:nparam)
+        lines(samples[,i], col=rainbow(nparam, alpha=0.75)[i])
+    xMin <- xMax <- yMax <- NULL
+    for(i in 1:nparam) {
+        d <- density(samples[,i])
+        xMin <- min(xMin,d$x); xMax <- max(xMax,d$x); yMax <- max(yMax, d$y) }
+    plot(1, xlim=c(xMin,xMax), ylim=c(0,yMax), type='n', main='Posterior Densities', xlab='', ylab='')
+    alpha_density <- 0.2
+    for(i in 1:nparam)
+        polygon(density(samples[,i]), col=rainbow(nparam, alpha=alpha_density)[i], border=rainbow(nparam, alpha=alpha_density)[i])
+    if(legend & !is.null(dimnames(samples)) & is.character(dimnames(samples)[[2]]))
+        legend(legend=dimnames(samples)[[2]], fill=rainbow(nparam, alpha=0.5), bty='n', x=legend.location)
+}
+
+
